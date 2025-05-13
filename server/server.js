cff00cdb3b4dee285eb0834d635352ba554d0e15
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2025-05-13 16:19:33 +0400

    frontend

--- server/server.js
+++ server/server.js
@@ -18,7 +18,10 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://swiftmart-chi.vercel.app",
+];
 
 app.post(`/stripe`, express.raw({ type: `application/json` }), stripeWebhooks);
 
